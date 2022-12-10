b718c165985b494bd4728fbdf875caba40f9ca64
Alperen Berat DURMUŞ <alperen@creditonline.eu>
2022-12-11 01:18:51 +0300

    Use ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), basicSsl()],
+  server: {
+    https: true
+  }
 })
